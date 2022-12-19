4a5b57a1058d02e404e0c26d7a2fd7bec44278cf
pneves1051 <79472414+pneves1051@users.noreply.github.com>
2022-12-19 11:18:12 -0300

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-csv_path = 'C:/Users/pedro/Google Drive (p185770@dac.unicamp.br)/maestro-v3.0.0-midi/maestro-v3.0.0/maestro-v3.0.0.csv'
-
-
-maestro_df = pd.read_csv(csv_path)
